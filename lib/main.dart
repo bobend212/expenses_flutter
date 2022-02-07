fad2e4aa4b111cd76ef6094ebe23f635b46d0ace
Mateusz Konopka (W) <mkonopkadev@gmail.com>
2022-02-07 15:48:29 +0100

    styling list of expenses

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
+import 'package:expenses_app/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -15,20 +16,70 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: '1', title: 'new shoes', amount: 99.99, date: DateTime.now()),
+    Transaction(id: '2', title: 'grocery', amount: 25.69, date: DateTime.now()),
+    Transaction(
+        id: '3', title: 'new shirt', amount: 140.45, date: DateTime.now()),
+    Transaction(id: '4', title: 'grocery', amount: 44.99, date: DateTime.now()),
+    Transaction(id: '5', title: 'bills', amount: 9.99, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Expenses App'),
       ),
-      body: Column(children: [
-        Card(
-          child: Text('CHART'),
-        ),
-        Card(
-          child: Text('List of entries'),
-        )
-      ]),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              child: Container(
+                child: Text('CHART'),
+                width: double.infinity,
+                color: Colors.amber[300],
+              ),
+              elevation: 5,
+            ),
+            Column(
+              children: transactions.map((transaction) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: EdgeInsets.all(5),
+                        child: Text(transaction.amount.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15)),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transaction.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15),
+                          ),
+                          Text(
+                            transaction.date.toString(),
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ]),
     );
   }
 }
